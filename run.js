3420d241844d6c7ee2243b106f6cff73c53a7985
Loralee Dawn <50155935+LoraleeSmith@users.noreply.github.com>
2019-12-19 20:55:51 -0800

    updates

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 M.toast({
     function newFunction() {
-    return 'Hi! My name is Loralee. I am a fullstack web developer with a background in Cosmetology, Business Management and Social Behavior.I attended the UCDavis Extension Web Developer 6 month bootcamp.';
+    return 'Thank you for your interest in me! Here is a small bit about me!';
 }
 var toastElement = document.querySelector('.toast');
 var toastInstance = M.Toast.getInstance(toastElement);
